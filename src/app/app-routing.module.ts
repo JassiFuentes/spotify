c6942ea1c3ef355f79f0439837523b82b6fda942
JassiFuentes <jassirafuentes@gmail.com>
2023-08-07 08:59:48 -0300

    update 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from '@modules/home/pages/home-page/home-page.component';
 
 //Aqui declaro mis rutas, la rutas seran objetos
 const routes: Routes = [
+  {
+    path:'auth', //TODO:
+    loadChildren:() => import(`./modules/auth/auth.module`).then(m => m.AuthModule),//Promesa
+  },
   //Ruta ppal o raiz
   {
     path:'', //TODO: localhost:4200/
     //en vez de usar la pdad component, usaremos loadChildren para la carga perezosa ya que las aplicaciones crecen, usamos una importacion dinamica y alli cargamos la ruta del modulo que usaremos
+    component: HomePageComponent,//hacemos las dos cosas cargar comoenente y modulo
     loadChildren:() => import(`./modules/home/home.module`).then(m => m.HomeModule),//Promesa
   }
 
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -6,5 +6,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+//Declaracion de variables
+  mainMenu: {//lo defino como objeto con los {} y tendra dos ppdades defaultOptions y accessLink
+    defaultOptions: Array<any>, //seran una lista de arrays lo que contendran
+    accessLink: Array<any>
+  } = { defaultOptions: [], accessLink: [] }//lo inicializo vacio
+
+  customOptions: Array<any> = []
+
+
+  ngOnInit(): void {//se usa pplmente para hacer llamados a servicios y url
+
+    //llenamos de datos
+    //lista de array que sus valores contienen la pdades de una opcion de ese menu
+    this.mainMenu.defaultOptions = [
+      {
+        name: 'Home',
+        icon: 'uil uil-estate',
+        router: ['/', 'auth']
+      },
+      {
+        name: 'Buscar',
+        icon: 'uil uil-search',
+        router: ['/', 'history']
+      },
+      {
+        name: 'Tu biblioteca',
+        icon: 'uil uil-chart',
+        router: ['/', 'favorites'],
+        query: { hola: 'mundo' }
+      }
+    ]
+
+    this.mainMenu.accessLink = [
+      {
+        name: 'Crear lista',
+        icon: 'uil-plus-square'
+      },
+      {
+        name: 'Canciones que te gustan',
+        icon: 'uil-heart-medical'
+      }
+    ]
+
+    this.customOptions = [
+      {
+        name: 'Mi lista º1',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º2',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º3',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º4',
+        router: ['/']
+      }
+    ]
+
+  }
 
 }
+
